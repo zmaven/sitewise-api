37e59d718cb5c8caa5274ad8806bf23cdf684d92
zmaven <zee@mavensoftwaredevelopment.com>
2023-08-28 23:33:41 +0800

    Update

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 const jsonServer = require('json-server')
 const bodyParser = require('body-parser');
-const clone = require('clone')
-const data = require('./db.json')
+const router = jsonServer.router("./db.json");
 
-const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
-
-// For mocking the POST request, POST request won't make any changes to the DB in production environment
-const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
-    _isFake: isProductionEnv
-})
 const middlewares = jsonServer.defaults()
 
 server.use(bodyParser.json({limit: '100mb', extended: true}))
