37b76bbcbc36c448ea9c36503b1a34efc4d4fe82
zmaven <zee@mavensoftwaredevelopment.com>
2023-08-28 22:57:41 +0800

    Update

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
 })
 const middlewares = jsonServer.defaults()
 
-server.use(bodyParser.json({limit: '10mb', extended: true}))
-server.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
+server.use(bodyParser.json({limit: '100bm', extended: true}))
+server.use(bodyParser.urlencoded({limit: '100mb', extended: true}))
 server.use(middlewares)
 
 server.use((req, res, next) => {
