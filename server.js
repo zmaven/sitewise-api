ca08a0dff4ae579a47a38bf7c9bbafb8e5f72a32
zmaven <zee@mavensoftwaredevelopment.com>
2023-08-28 23:49:44 +0800

    Update

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-const jsonServer = require('json-server')
+// JSON Server module
+const jsonServer = require("json-server");
 const bodyParser = require('body-parser');
+const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
 
-const server = jsonServer.create()
-const middlewares = jsonServer.defaults()
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
 
 server.use(bodyParser.json({limit: '100mb', extended: true}))
 server.use(bodyParser.urlencoded({limit: '100mb', extended: true}))
-server.use(middlewares)
-
-server.use(router)
-server.listen(process.env.PORT || 8000, () => {
-    console.log('JSON Server is running')
-})
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
+server.listen(3000, () => {
+ console.log("JSON Server is running");
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
