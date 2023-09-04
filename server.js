739ae78587bce951e4e68bf5f6212ff2ea12f71f
zmaven <zee@mavensoftwaredevelopment.com>
2023-09-05 06:54:44 +0800

    Update

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 // JSON Server module
 const jsonServer = require("json-server");
+const fs = require("fs");
+const os = require("os");
+
+fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
+  if (err) console.log(err);
+  else console.log("copy file succeed to" + os.tmpdir());
+});
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.resolve(os.tmpdir() + "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
